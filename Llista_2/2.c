9153fcb4cfadf6346f665676d6b8fee66745e7d6
ARNAU QUINDOS SANCHEZ <aquindsa10.alumnes@ub052229.mat.ub.edu>
2017-10-18 18:56:58 +0200

    Problemes 2,3 actualitzats (Pea), falta comprovacio 3

--- Llista_2/2.c
+++ Llista_2/2.c
@@ -1,95 +1,132 @@
-/* 
-Resolucio sistemes d'equacions amb pivotatge parcial
+/* Ax = b
+ * Dimensio n
+ * Llegeix n, A i b
+
 */
 
 #include<stdio.h>    
 #include<stdlib.h>  
-#include<time.h>
+#include <time.h>  
 #include<math.h>
-
+int n;
+double tolerancia;
+void printmatrix(double** A){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%+1.4f       ", A[i][j]);
+        }
+        printf("\n");
+    }
+     printf("\n");
+}
+/*Eliminació gaussiana amb pivotatge parcial*/
+int elimGauss(double**A, double *b){
+    double max, *vauxiliar, m;
+    int k,v,i,j;
+    /*Vector auxiliar per a l'intercanvi de files: Serà de mida n+1 per guardar també bk.*/
+    vauxiliar = (double *)malloc((n+1)*sizeof(double));
+    /*Cal fer n-1 etapes: */
+    for(k=0;k<n-1;k++){
+        /*Busquem v pertanyent a {k+1,....,n} tq avk(n) sigui el màxim: */
+        max = A[k][k];
+        v = k;
+        for(i=k+1;i<n;i++){
+            if(fabs(A[i][k]) > fabs(max)){
+                max = A[i][k];
+                v = i; 
+            }
+        }
+        /*Si el màxim es menor a la tolerància, ja hem acabat:*/
+        if(fabs(max) < tolerancia) return 1;
+        /*Intercanviem les files k i v (a partir de la columna k, ja que les altres seran 0. */
+        /*Guardem fila k a vauxiliar: */
+        for(i=k;i<n;i++){
+            vauxiliar[i] = A[k][i];
+        }
+        vauxiliar[n] = b[k];
+        /*Passem la fila v a k:*/
+        for(i=k;i<n;i++){
+            A[k][i] = A[v][i];
+        }
+        b[k] = b[v];
+        /*Passem el vector auxiliar, on guardem la fila k, a v:*/
+        for(i=k;i<n;i++){
+            A[v][i] = vauxiliar[i];
+        }
+        b[v] = vauxiliar[n];
+        /*Un cop hem realizar el canvi de files, calculem per a cada fila i€{k+1,.....,n} mik  i fem la resta:*/
+        for(i=k+1;i<n;i++){
+            /*Calculem mik*/
+            m = A[i][k] / A[k][k];
+            /*Ja sabem que efectuant la resta a i=k obtenim 0:*/
+            A[i][k] = 0;
+            /*Per a cada j€{k+1,.....,n} fem la resta corresponent, i també per a bi*/
+            for(j=k+1;j<n;j++) A[i][j] -= m*A[k][j]; 
+            b[i] -= m*b[k];
+        }
+    }
+    return 0;
+}
+/*Resolució del sistema triangular per substitució endarrera:*/
+int resoltriangular(double **A, double *b){
+    int i, j;
+    double sumatori;
+    /*xn:*/
+    b[n-1] = b[n-1]/A[n-1][n-1];
+    for(i=n-2;i>=0;i--){
+        /*Sumatori*/
+        sumatori = 0;
+        for(j=i+1;j<n;j++) sumatori += A[i][j]*b[j];
+        /*Restem b[i] (és a dir, actualment és ci) - sumatori*/
+        b[i] = b[i] - sumatori;
+        /*Si Aii es major a la tolerancia, dividim per Aii:*/
+        if(A[i][i] > tolerancia){
+        b[i] = b[i]/A[i][i];
+        }else{
+            return 2;
+        }
+    }
+    return 0;
+}
 int main(void)
 {
-    double **A, *aux, a, m, sum;
-    int n, i, j, k, l, t, it, jt, r;
-    clock_t init, fint;
-
-    srand( time(NULL) );
-    printf("Introdueix n: ");
-    scanf("%d", &n);
-    
-    /*Reservem memoria per la matriu*/
-    A = (double **)malloc( n*sizeof(double *) );
-    if ( A==NULL) return 2;
-    for (i=0; i<n; i++) {
-        A[i] = (double *)malloc( (n+1)*sizeof(double) );
-        if ( A[i]==NULL) return 3; 
-    }
-    
-    /*Emplenem la matriu*/
-    for (i=0; i<n; i++) {
-        for (j=0; j<n+1; j++){
-            if(j!=n){
-                printf("Introdueix A%d%d ",i,j);
-                scanf("%lf", &a);
-            }else{
-                printf("Introdueix C%d ",i);
-                scanf("%lf", &a);
-            }
-            A[i][j] = a;
-        } 
-    }
-    
-    /*Eliminacio Gauss*/
-    for (k=0; k<n-1; k++) {
-        /*Pivotatge
-        for(l=0; l<n; l++){
-            for(it=k+1, r=k; it<n; it++){
-              if(fabs(A[it][k])>fabs(A[r][k])){
-                  r=i;
-              }  
-            } 
-            if(r!=k){
-                aux=A[r];
-                A[r]=A[k];
-                A[k]=aux;
-            }
+   double **A, *b;
+   int i, j;
+   tolerancia = 1.e-14;
+   /*Llegim A*/
+   printf("Escriu l'enter n:");
+   scanf("%d", &n);
+   A = (double **)malloc(n*sizeof(double*));
+   for(i=0;i<n;i++){
+       A[i] = (double *)malloc(n*sizeof(double));
+       for(j=0;j<n;j++){
+        printf("Escriu A%d%d:",i,j);
+        scanf("%le", &A[i][j]);
         }
-        Final pivotatge*/
-        if(fabs(A[k][k])<1.e-14) return 1;
-        for (i=k+1; i<n; i++) {
-            m = A[i][k]/A[k][k]; 
-            for (j=k; j<n+1; j++){
-                A[i][j]= A[i][j] - m*A[k][j];
-            }
-        }
+   }
+   /*Llegim b*/
+   b = (double *)malloc(n*sizeof(double));
+   for(i=0;i<n;i++){
+        printf("Escriu b%d:",i);
+        scanf("%le", &b[i]);
     }
     
-    /*Imprimim la matriu*/
-    for (i=0; i<n; i++) {
-        for (j=0; j<n+1; j++){
-            printf("%2lf ",A[i][j]);
-        } 
-        printf("\n");
-    }
+    printf("\n");
+    printmatrix(A);
     
-    /*Resolem la matriu superior triangular*/
-    for(i=n-1; i>=0; i--){
-		for(j=i+1, sum=0.; j<n; j++){
-           sum+=A[i][j]*A[j][n]; 
-        } 
-        A[i][n]=1./A[i][i]*(A[i][n]-sum);
-    }
+    /*Fem eliminació Gaussiana. A esdevindrà A(k) i b esdevindrà c */
+    elimGauss(A,b);
+    printf("Matriu triangulada\n");
+    printmatrix(A);
     
-    /*Imprimim les solucions*/
-    for (i=0; i<n; i++) {
-        printf("x%1d: %4lf ",i,A[i][n]);
-        printf("\n");
-    }
+    /*Fem sustitució triangular. c esdevindrà x (solucio)*/
+    resoltriangular(A,b);
     
-    /*Alliberem memoria matriu*/
-    for (i=0; i<n; i++) free(A[i]);
-    free(A);
-
+    /*Mostrem els resultats:*/
+    printf("Solucions:\n");
+    for(i=0;i<n;i++){
+        printf("x%d = %+.7le\n", i+1, b[i]);
+    }
     return 0;
-
 }
