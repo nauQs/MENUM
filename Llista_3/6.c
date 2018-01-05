ab226a2a7b481f9111845ee112b499f55a4999eb
nauQs <arnauquindos@MacBook-Pro-de-Arnau.local>
2018-01-05 12:25:47 +0100

    Llista 3 acabada (falta completar 2/3)

--- Llista_3/6.c
+++ Llista_3/6.c
@@ -4,47 +4,50 @@
 
 double fun(double x){
     double y;
-    y = exp(x);
+    y = 1/(1.+x);
     return y;
 }
-double F0(double a, double h){
-    double y;
-    y = (fun(a+h)-fun(a-h))/(2*h);
+double trap(int n, double a, double h){
+    double y = 0.;
+    double x;
+    for (int i=1;i<n;i++){
+        x = a + i*h;
+        y += h*fun(x);
+    }
+    y += h*(fun(a)+fun(x+h))/2;
     return y;
 }
 int main (void){
-    int n, i, j, q, end;
-    double **M,a,h,prec, res;
+    int n, i, j, k, q, end;
+    double **M,a,b,h,prec,res;
     q = 2.; /*Pas de disminucio*/
     prec = 1.e-10; /*Precisio desitjada*/
-    a = 1.;
+    a = 0.;
+    b = 1.;
     end = 0;
+    k = 10;
     printf("Digues n: ");
     scanf("%d",&n);
-    printf("Digues h: ");
-    scanf("%le", &h);
-    
-    M = (double**)malloc(n*sizeof(double*));
-
-    for(i=0;i<n;i++){
-        M[i] = (double*)malloc(n*sizeof(double));
+    h = (b-a)/n;
+    M = (double**)malloc(k*sizeof(double*));
+    for(i=0;i<k;i++){
+        M[i] = (double*)malloc(k*sizeof(double));
     }
-    /*Ara tenim una matriu amb tots els parells (xi,fi) de 0 a n*/
+    
     /*Fem la diferenciacio*/
-    M[0][0] = F0(a,h);
+    M[0][0] = trap(n,a,h);
     printf("%.10e\n",M[0][0]);
-    for(i=1;i<n;i++){
-        M[i][0] = F0(a,h/pow(q,i));
+    for(i=1;i<k;i++){
+        M[i][0] = trap(n*pow(q,i),a,h/pow(q,i));
         for(j=1;j<i+1;j++){
             M[i][j] = (pow(q,2*j)*M[i][j-1]-M[i-1][j-1])/(pow(q,2*j)-1);
             if (fabs(M[i][j]-M[i][j-1])<prec){
-                /*printf("\nFINAL: %.10e %.10e\n", M[i][j],M[i][j-1]);*/
                 end = 1;
                 res = M[i][j];
             }
         }
         for(j=0;j<i+1;j++){
-            printf("%.10e ",i,j,M[i][j]);
+            printf("%.10e ",M[i][j]);
         }
         printf("\n");
         if(end){
